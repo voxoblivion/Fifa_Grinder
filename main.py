57c8d8e065c8c989b80e285cdf841a3fe8c4c893
voxoblivion <tomjephcote@hotmail.co.uk>
2018-11-09 12:40:03 +0000

    new errors

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class single_player_seasons():
         self.games = 0
         self.ingame = True
         self.teams = 0
-        self.team = team2
+        self.team = team_loyalty
 
     def movetosecondhalf(self):
         ctypes.windll.user32.SetCursorPos(962, 323)
@@ -20,7 +20,7 @@ class single_player_seasons():
     def movetosquadscreen(self):
         self.games += 1
         ctypes.windll.user32.SetCursorPos(287, 327)
-        press_key(0x1C, 6, 5)
+        press_key(0x1C, 6, 10)
         time.sleep(5)
         if is_on_screen(bbox=newseason_box, condition='ENTRY REQUIREMENTS') is True:
             ctypes.windll.user32.SetCursorPos(287, 327)
@@ -29,58 +29,63 @@ class single_player_seasons():
             time.sleep(2)
             press_key(0x1C, 2, 5)
             time.sleep(2)
+        print("SQUAD SCREEN")
 
     def fixplayerinjuries(self):
         img = ImageGrab.grab(bbox=injuredplayers_box)
         injured_players = (image_to_string(img, lang='eng'))
         injured_players_list = injured_players.split(', ')
-        if injured_players_list[0] in team.keys():
+        if injured_players_list[0] in self.team.keys():
+            print(injured_players_list)
             press_key(0x1C, 1, 5)
             time.sleep(2)
             for i in injured_players_list:
-                ctypes.windll.user32.SetCursorPos(team[i][0] + 30, team[i][1] + 30)
+                ctypes.windll.user32.SetCursorPos(self.team[i][0] + 30, self.team[i][1] + 30)
                 time.sleep(2)
                 press_key(0x1F, 1, 5)
                 press_key(0x1C, 2, 5)
                 press_key(0x01, 1, 5)
             press_key(0x01, 1, 5)
             press_key(0x1C, 1, 5)
+        print("PLAYER INJURERS SORTED")
 
     def checkfitness(self):
-        if (self.games % 3) == 0:
-            team_played = 0
+        if (self.games % 2) == 0:
             press_key(0xD0, 1, 3, hold_time=0.1)
             press_key(0x1C, 1, 4)
             press_key(0x20, 1, 2)
             press_key(0x1C, 1, 2)
+            time.sleep(5)
             ctypes.windll.user32.SetCursorPos(755, 510)
             time.sleep(3)
             press_key(0x1C, 1, 10)
-            team = teams[teams.index(self.team) - 1]
+            self.team = teams[teams.index(self.team) - 1]
+            print(self.team)
+            print("TEAM SWITCHED")
 
     def movetonextgame(self):
         press_key(0x39, 1, 3)
         press_key(0x1C, 9, 5)
-        time.sleep(10)
-        print("ESC")
+        time.sleep(20)
         press_key(0x01, 1, 4)
+        print("GAME STARTED")
 
 
 def press_key(hex_no, amount, refresh_time, hold_time=0.5):
     for i in range(amount):
-        if hex_no == 0x1C:
-            print(i)
         PressKey(hex_no)
         time.sleep(hold_time)
         ReleaseKey(hex_no)
-        print("a completed")
         time.sleep(refresh_time)
 
 
 def is_on_screen(bbox, condition):
     screen = ImageGrab.grab()
-    if (image_to_string(screen.crop(bbox), lang='eng')) == condition:
-        return True
+    screen_text = (image_to_string(screen.crop(bbox), lang='eng'))
+    if screen_text != "":
+        print(screen_text)
+        if screen_text == condition:
+            return True
     else:
         return False
 
@@ -96,20 +101,23 @@ team1 = {"Otto": (779, 61), "Mello": (629, 137), "Fraser": (968, 136),
 team2 = {"Takagi": (779, 61), "Puri": (629, 137), "Edwards": (968, 136),
             "Ariza": (443, 180), "Hawkridge": (1152, 186),
             "Thorsem": (805, 244), "Bermingham": (447, 376), "Turton": (1174, 375),
-            "Han Pengei": (661, 390), "Sowunmi": (953, 388), "Broda": (800, 489)}
-team_loyalty = {"Zapata": (779, 61), "Luis Alberto": (629, 137), "Ramirez": (968, 136),
-            "Gervinho": (443, 180), "Suso": (1152, 186),
-            "Badelj": (805, 244), "Durmisi": (447, 376), "Rafael": (1174, 375),
-            "Wauge": (661, 390), "Romagnoli": (953, 388), "Karnezis": (800, 489)}
+            "Han Pengfei": (661, 390), "Sowunmi": (953, 388), "Broda": (800, 489)}
+team_loyalty = {"Jonas": (779, 61), "Pizzi": (629, 137), "Bruno Fernandes": (968, 136),
+            "Payet": (443, 180), "Quaresma": (1152, 186),
+            "William Carvalho": (805, 244), "Ribery": (447, 376), "Dost": (1174, 375),
+            "Ruffier": (661, 390), "Rami": (953, 388), "Lopes": (800, 489)}
 teams = [team1, team2]
-team = team2
 
 # TODO restart process when disconnect occurs
 # TODO fix injured players ingame
-
+'''
+Issue occurs when game has been running for a while and key presses no longer function 
+as intended. Could be fixed with a soft reset.
+'''
 
 if __name__ == '__main__':
     game = single_player_seasons()
+    game.team = team2
     while True:
         game.ingame = True
         if is_on_screen(halftime_box, '1.5:lll]') is True:
@@ -123,3 +131,6 @@ if __name__ == '__main__':
             game.ingame = False
         if game.ingame is True:
             press_key(0x26, 1, 1)
+'''
+lllllllllllllllllLLL
+'''
--- test.py
+++ test.py
@@ -3,17 +3,37 @@ from pytesseract import image_to_string
 import time
 import ctypes
 from direct_keys import *
+from main import press_key
 
 team1 = {"Otto": (779, 61), "Mello": (629, 137), "Fraser": (968, 136),
             "Marseiler": (443, 180), "Hansen": (1152, 186),
-            "Fujita": (805, 244), "Poaty": (447, 376), "Frantsen": (1174, 375),
+            "Arthur": (805, 244), "Poaty": (447, 376), "Frantsen": (1174, 375),
             "Empereur": (661, 390), "Pask": (953, 388), "Brunst": (800, 489)}
 team2 = {"Takagi": (779, 61), "Puri": (629, 137), "Edwards": (968, 136),
             "Ariza": (443, 180), "Hawkridge": (1152, 186),
             "Thorsem": (805, 244), "Bermingham": (447, 376), "Turton": (1174, 375),
             "Han Pengei": (661, 390), "Sowunmi": (953, 388), "Worra": (800, 489)}
+team = team1
 img = ImageGrab.grab()
 entryreqs_box = (1157, 259, 1360, 290)
-img3 = img.crop(entryreqs_box)
-print(image_to_string(img3, lang='eng'))
-d
+fulltime_box = (710, 88, 891, 122)
+halftime_box = (938, 136, 1007, 169)
+injuredplayers_box = (480, 416, 1028, 449)
+img1 = img.crop(halftime_box)
+
+time.sleep(5)
+img = ImageGrab.grab(bbox=injuredplayers_box)
+injured_players = (image_to_string(img, lang='eng'))
+injured_players_list = injured_players.split(', ')
+print(injured_players_list)
+if injured_players_list[0] in team.keys():
+    press_key(0x1C, 1, 5)
+    time.sleep(2)
+    for i in injured_players_list:
+        ctypes.windll.user32.SetCursorPos(team[i][0] + 30, team[i][1] + 30)
+        time.sleep(2)
+        press_key(0x1F, 1, 5)
+        press_key(0x1C, 2, 5)
+        press_key(0x01, 1, 5)
+    press_key(0x01, 1, 5)
+    press_key(0x1C, 1, 5)
